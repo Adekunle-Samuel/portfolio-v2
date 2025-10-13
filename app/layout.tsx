76bf3ef35f1eee784903b2ec01adbcdc3df9dec2
Adekunle-Samuel <adekunlesamuel204@gmail.com>
2025-10-12 22:45:41 -0400

    Enhance metadata for SEO and social sharing in layout.tsx
    - Added Open Graph metadata for improved social media sharing.
    - Included Twitter card metadata for better visibility on Twitter.
    - Updated existing metadata to ensure consistency across platforms.

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,19 @@ import StyledComponentsRegistry from "@/lib/registry";
 export const metadata: Metadata = {
   title: "Sam Adekunle - Portfolio",
   description: "Designer working at the intersection of product, brand and development.",
+  openGraph: {
+    title: "Sam Adekunle - Portfolio",
+    description: "Designer working at the intersection of product, brand and development.",
+    url: "www.thesamadekunle.com", // Replace with your actual domain
+    siteName: "Sam Adekunle Portfolio",
+    locale: "en_US",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Sam Adekunle - Portfolio",
+    description: "Designer working at the intersection of product, brand and development.",
+  },
 };
 
 export default function RootLayout({

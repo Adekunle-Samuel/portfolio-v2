edb1b7261b6e293ba2fea52b9031f400df2f75b0
Adekunle-Samuel <adekunlesamuel204@gmail.com>
2025-12-04 01:59:53 -0500

    refactor: remove `shouldForwardProp` prop from `StyleSheetManager` component.

--- lib/registry.tsx
+++ lib/registry.tsx
@@ -22,10 +22,7 @@ export default function StyledComponentsRegistry({
   if (typeof window !== 'undefined') return <>{children}</>
 
   return (
-    <StyleSheetManager
-      sheet={styledComponentsStyleSheet.instance}
-      shouldForwardProp={(prop) => prop !== 'disableTransition'}
-    >
+    <StyleSheetManager sheet={styledComponentsStyleSheet.instance}>
       {children}
     </StyleSheetManager>
   )
